0f8d95f42bb26696c150ebdccaf185700b6ff9ac
MatsunamiDaiki <lax.md0@gmail.com>
2020-07-10 11:04:52 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,7 +40,6 @@ $(function(){
   $('.form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
-    console.log(formData)
     let url = $(this).attr('action');
     $.ajax({
       url: url,
